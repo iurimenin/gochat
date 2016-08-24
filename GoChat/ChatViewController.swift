40b3fa01107d375ad2f25619c9d0f112ddb98021
iurimenin <iuri020293@>
2016-08-24 01:20:53 -0300

    Adicionado tratamento dos botões de media no chat

--- GoChat/ChatViewController.swift
+++ GoChat/ChatViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import JSQMessagesViewController
+import MobileCoreServices
+import AVKit
 
 class ChatViewController: JSQMessagesViewController {
 
@@ -17,20 +19,80 @@ class ChatViewController: JSQMessagesViewController {
         super.viewDidLoad()
         self.senderId = "1"
         self.senderDisplayName = "Iuri"
-        // Do any additional setup after loading the view.
     }
     
     override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
-        
-        print("didPressSendButton")
-        print("\(text)")
         messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text))
+        collectionView.reloadData()
     }
     
     override func didPressAccessoryButton(sender: UIButton!) {
         print("didPressAccessoryButton")
+        
+        let sheet = UIAlertController(title: "Mensagens de Mídia", message: "Selecione uma mídia", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        self.presentViewController(sheet, animated: true, completion: nil)
+        let cancel = UIAlertAction(title: "Cancelar", style: .Cancel) { (UIAlertAction) in
+            
+        }
+        
+        let photoLibrary = UIAlertAction(title: "Fotos", style: .Default) { (UIAlertAction) in
+            self.getMediaFrom(kUTTypeImage)
+        }
+        
+        let videoLibrary = UIAlertAction(title: "Vídeos", style: .Default) { (UIAlertAction) in
+            self.getMediaFrom(kUTTypeMovie)
+        }
+        
+        sheet.addAction(photoLibrary)
+        sheet.addAction(videoLibrary)
+        sheet.addAction(cancel)
     }
     
+    func getMediaFrom(type: CFString) {
+        
+        let mediaPicker = UIImagePickerController()
+        mediaPicker.delegate = self
+        mediaPicker.mediaTypes = [type as String]
+        self.presentViewController(mediaPicker, animated: true, completion: nil)
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        return bubbleFactory.outgoingMessagesBubbleImageWithColor(.blackColor())
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
+        return cell
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, didTapMessageBubbleAtIndexPath indexPath: NSIndexPath!) {
+        
+        let message = messages[indexPath.item]
+
+        if message.isMediaMessage {
+        
+            if let mediaItem = message.media as? JSQVideoMediaItem {
+                
+                let player = AVPlayer(URL: mediaItem.fileURL)
+                let playerViewController = AVPlayerViewController()
+                playerViewController.player = player
+                self.presentViewController(playerViewController, animated: true, completion: nil)
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -52,14 +114,20 @@ class ChatViewController: JSQMessagesViewController {
 
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension ChatViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let picture = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let photo = JSQPhotoMediaItem(image: picture)
+            messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: photo))
+        } else if let video = info[UIImagePickerControllerMediaURL] as? NSURL {
+            let videoItem = JSQVideoMediaItem(fileURL: video, isReadyToPlay: true)
+            messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: videoItem))
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+        collectionView.reloadData()
     }
-    */
-
 }
